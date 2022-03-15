65ca1f5a1e7cb775470c16a3a92bd0dcb7496aa8
Rachel LeBoeuf <rachelnleboeuf@gmail.com>
2022-03-14 22:33:36 -0400

    added various JS functions, Jquery, edits for quiz functionality

--- script.js
+++ script.js
@@ -1,17 +1,64 @@
+//CHRONOLOGICAL ORDER
 var startButton = document.querySelector('.button');
 var quizQuestions = document.querySelector('.quizQuestions');
+var timerEl = document.querySelector('#timer');
+var possibleAnswer = document.querySelector('#possibleAnswer');
+var endQuiz = document.querySelector('#endQuiz');
+var initialsInput = document.querySelector('#initials');
+var timer = 0;
+var MAXTIME = 75;
+var newTimer = null;
 var currentQuestion = 0;
+var currentAnswer = '';
 
 
+endQuiz.style.display = "none";
+function reportAnswer(event) {
+  console.log('launch event', event.textContent);
+  currentAnswer = event.textContent;
+  evaluateAnswer();
+  setTimeout(function () {
+    nextQuestion();
+  }, 1000);
+
+}
+function evaluateAnswer() {
+  possibleAnswer.style.display = "inline";
+  if (questions[currentQuestion].correct === currentAnswer) {
+    possibleAnswer.textContent = 'correct';
+  } else {
+    possibleAnswer.textContent = 'wrong';
+    timer -= 5
+  }
+}
+//OBJECT WAS "DESTRUCTERED" IN THE PARAMETER LIST- 
+function questionComponent({ question, answers }) {
+  var questionText = '<div>' + question + '</div>'
+  var buttonList = ''
+  for (const answer of answers) {
+    buttonList += '<button onclick="reportAnswer(this)">' + answer + '</button><br>'
+  }
+
+  return questionText + buttonList
+
+}
+// time argument is in milliseconds. 1 = 1000 milliseconds.
+function Timer(time) {
+  timer = MAXTIME
+  return setInterval(function () {
+    timerEl.textContent = timer--
+  }, time)
+
+}
 
 // Arrays use indexes, ex: questions[0] === {question: "q1", ...}
 var questions = [
 
   // Objects use keys, ex: questions[0].question === "q1"
   { // index 0
-    question: "q1",
-    answers: ["a", "b", "c", "d"],
-    correct: "b"
+    question: "What is the abbrivation for JavaScript?",
+    answers: ["J", "JS", "JST", "Java"],
+    correct: "JS"
   },
 
   { // index 1
@@ -33,53 +80,45 @@ var questions = [
   }
 ];
 
+
+
+
 function beginQuiz() {
+  //Add Timer here
+  if (!newTimer) {
+    newTimer = Timer(1000)
+  }
+  quizQuestions.style.display = "block";
+  possibleAnswer.textContent= '';
   console.log("is my function working");
   startButton.setAttribute("class", "button hidden");
-  questions.addEventListener("button").addEventListener("click", startButton);
+  console.log(questions.length);
+  var aQuestion = questionComponent(questions[currentQuestion])
+  quizQuestions.innerHTML = aQuestion
+
+
+
+
+}
+function endQuizFN() {
+  quizQuestions.style.display = "none";
+  possibleAnswer.style.display = "none";
+  endQuiz.style.display = "inline";
 }
 
 
-
-
-
-
-
-
-var questionTitle = document.createElement('h1');
-questionTitle.textContent = questions[0].question;
-quizQuestions.appendChild(questionTitle);
-
-var questionTitle = [
-  {
-    question: "",
-    answers: {
-      a: "",
-      b: "",
-      c: ""
-    },
-    correctAnswer: ""
+function nextQuestion() {
+  currentQuestion++
+  if (currentQuestion < questions.length) {
+    beginQuiz();
+  } else {
+    endQuizFN();
   }
-];
 
 
 
 
-
-
+}
 
 
 startButton.addEventListener("click", beginQuiz);
-
-
-
-// var timeleft = 75;
-// var downloadTimer = setInterval(function(){
-//   if(timeleft <= 0){
-//     clearInterval(downloadTimer);
-//     document.getElementById("countdown").innerHTML = "Finished";
-//   } else {
-//     document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-//   }
-//   timeleft -= 1;
-// }, 1000);
